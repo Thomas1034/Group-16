5951322c5dea70bf73c06b5bc84123060edd5f28
cosjulian <89463345+Cosjulian@users.noreply.github.com>
2024-11-19 12:47:19 -0500

    Menu

--- frontend/src/homepage/NavBar.tsx
+++ frontend/src/homepage/NavBar.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { AppBar, Toolbar, Typography, InputBase, styled, alpha, Avatar, Stack, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, InputBase, styled, alpha, Avatar, Stack, Container, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
+
+const settings = ['Profile', 'Logout'];
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
@@ -50,7 +52,37 @@ const Search = styled('div')(({ theme }) => ({
 // will need id later
 function NavBar()
 {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   const navigate = useNavigate();
+
+  const PerformSearch = (e: any) => 
+  {
+    const url = `tbd/?q=${e.target.value}`;
+    return fetch(url, {
+      method: "GET"
+      }).then((res) => {
+          if (res.status === 200) return res.json();
+          else if (res.status === 404) return "";
+          else throw new Error(`Got unexpected reponse status ${res.status} from search endpoint`);
+      });
+  }
     return (
         <AppBar position="fixed" style={{backgroundColor: "white"}}>
         <Toolbar>
@@ -66,9 +98,33 @@ function NavBar()
                 sx={{flexGrow: 1,}}
                 placeholder="Search…"
                 inputProps={{ 'aria-label': 'search' }}
+                onChange = {(e) => PerformSearch(e)}
+
               />
             </Search>
-            <Avatar style={{position:"relative", justifyContent: "right"}} alt="szum" src="https://online.stanford.edu/sites/default/files/styles/cropped_square/public/instructors/T001798-Szumlanski-Sean.jpg"/>
+            <Avatar style={{position:"relative", justifyContent: "right"}} alt="szum" src="https://online.stanford.edu/sites/default/files/styles/cropped_square/public/instructors/T001798-Szumlanski-Sean.jpg" onClick={handleOpenUserMenu}/>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Stack>
         </Toolbar>
       </AppBar>
