d116e075eba9340e149c330b322d5fc2e043a0c0
Thomas Kern <142808316+Thomas1034@users.noreply.github.com>
2024-11-30 19:27:40 -0500

    Single Manager Route

--- backend/src/controllers/managerController.ts
+++ backend/src/controllers/managerController.ts
@@ -6,6 +6,10 @@ interface StringBearer {
     page?: string;
 }
 
+interface IdHolder {
+    id?: string;
+}
+
 export const create = async(req: Request, res: Response) => {
     try {
         const { name, image, url } = req.body;
@@ -53,3 +57,29 @@ export const getAll = async(req: Request<{}, {}, {}, StringBearer>, res: Respons
     }
 };
 
+
+export const get = async(req: Request<IdHolder, {}, {}, {}>, res: Response) => {
+    try {
+        const { query } = req;
+				const id = req.params.id;
+
+        if(!id) {
+            res.status(400).json({error: "Missing request ID"});
+            return;
+        }
+				
+				// TODO
+				var manager = await ContactManager.find({_id: id}).then(function (dataArray) {
+					return dataArray?.length == 1 ? dataArray[0] : undefined;
+				});
+				
+				if (undefined == manager) {
+						res.status(404).json({error: "Contact manager not found"});
+				} else {
+						res.status(200).json(manager);
+				}
+    } catch (error) {
+				console.log("managerController.ts encountered an unexpected error:\n" + error);
+        res.status(500).json({error: "Internal server error."});
+    }
+};
