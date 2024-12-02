62517728dfc2a0db823e38935201050f1aff556e
SilverMight <joeyzee1234@gmail.com>
2024-12-02 13:45:26 -0500

    Add delete logic and route

--- backend/src/controllers/managerController.ts
+++ backend/src/controllers/managerController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ContactManager from '../models/ContactManager';
+import Review from '../models/Review';
 import mongoose from 'mongoose';
 
 interface StringBearer {
@@ -14,7 +15,7 @@ export const create = async(req: Request, res: Response) => {
     try {
         const { name, url } = req.body;
         const author = req.user_id;
-				
+
         if(!name || !url || !author) {
             res.status(400).json({error: "Missing required fields"});
             return;
@@ -80,6 +81,46 @@ export const get = async(req: Request<IdHolder, {}, {}, {}>, res: Response) => {
 
         res.status(201).json(manager);
 
+    } catch (error) {
+        console.log("managerController.ts encountered an unexpected error:\n" + error);
+        res.status(500).json({error: "Internal server error."});
+    }
+};
+
+export const deleteManager = async(req: Request<IdHolder, {}, {}, {}>, res: Response) => {
+    try {
+        const id = req.params.id;
+        const author = req.user_id;
+
+        // Check if the ID is a valid ObjectId
+        if(!id || !mongoose.Types.ObjectId.isValid(id)) {
+            res.status(400).json({error: "Invalid ID"});
+            return;
+        }
+
+        const managerObjId = new mongoose.Types.ObjectId(id); 
+        var manager = await ContactManager.findById(managerObjId);
+
+        if(manager === null) {
+            res.status(404).json({error: "Manager not found"});
+            return;
+        }
+
+        // Check if the author of the manager is the same as the user
+        if(manager.author != author) {
+            res.status(401).json({error: "Unauthorized"});
+            return;
+        }
+
+        // Delete all ratings associated with the manager
+        await Review.deleteMany({contactManagerId: managerObjId});
+
+        await manager.deleteOne();
+
+        // We are leaving the image (if any) on the server, for now...
+
+
+        res.sendStatus(204);
     } catch (error) {
         console.log("managerController.ts encountered an unexpected error:\n" + error);
         res.status(500).json({error: "Internal server error."});
